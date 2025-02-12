250a20e7737c41215735957a1573d1db55e1ed83
shubham043-hash <waghmareshubham043@gmail.com>
2025-02-12 15:03:28 +0530

    Create DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,38 @@
+"use Strict" //treat all js code as strict
+
+"use strict"; //treat all js code as newer version
+
+// alert (3 + 3) // we are using node.js,not browser
+
+
+console.log(3+3); 
+console.log("shubham");
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// primitive dataTypes
+// *******************
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique value
+
+
+
+// object
+
+console.log(typeof undefined); // type is undefined  IMP INTERVIEW QUESTION
+console.log(typeof null); // type is object IMP INTERVIEW QUESTION
+
+
+
+
+
+
