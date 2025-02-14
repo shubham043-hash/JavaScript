e8e260ed26541dcfaa39a673674eae7d9bf5bd1a
shubham043-hash <waghmareshubham043@gmail.com>
2025-02-14 16:20:40 +0530

    Create operations.js

---
+++ operations.js
@@ -0,0 +1,32 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//**************operations************** */
+
+
+let str1 = "hello"
+let str2 = " shubham"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log(typeof "1" + 2);
+console.log(typeof 1 + "2");
+console.log(typeof "1" + 2 + 2);
+console.log(typeof 1+2+"2");
+
+
+
+console.log((3 + 4) * (5 % 3)); //use parenthesis when you use multiple value
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
