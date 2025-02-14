3e0757581aa395386b5b06b56eb9a26bac9895f4
shubham043-hash <waghmareshubham043@gmail.com>
2025-02-14 16:04:24 +0530

    Create Conversion.js

---
+++ Conversion.js
@@ -0,0 +1,13 @@
+let score = undefined
+
+//we can use in two type of ways
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33abc" => NaN
+// true =>1; false => 0
